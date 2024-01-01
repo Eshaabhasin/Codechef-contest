70f0e6d064749d58d98e218c90106d6fbc87fc9b
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2024-01-01 20:49:35 +0530

    Add files via upload

---
+++ christmas.cpp
@@ -0,0 +1,16 @@
+//Problem Link:https://www.codechef.com/problems/CHRISTGREET
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int X;
+    cin>>X;
+    if(X==25){
+        cout<<"Christmas"<<endl;
+    }
+    else{
+        cout<<"Ordinary";
+    }
+	// your code goes here
+
+}
