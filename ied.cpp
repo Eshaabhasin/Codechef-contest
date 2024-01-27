d7ddb1d16c0be4182ec3b1329911937b0958ba75
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2024-01-27 21:40:14 +0530

    Add files via upload

---
+++ ied.cpp
@@ -0,0 +1,14 @@
+//Problem Link: https://www.codechef.com/problems/IED
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    int z,y;
+    z=a*c;
+    y=b*c;
+    cout<<max(z,y)<<endl;
+	
+
+}
