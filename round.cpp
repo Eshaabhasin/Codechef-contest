b43d45e1b97e1be8a3c867f9d02682b46dd572a4
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2024-01-30 21:35:19 +0530

    Add files via upload

---
+++ round.cpp
@@ -0,0 +1,16 @@
+//PROBLEM LINK:https://www.codechef.com/problems/CODECHEF
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin>>N;
+    if(N==4){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+	// your code goes here
+
+}
