947f7754900fe69811cf8aa3c15e7915117b0c01
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2024-01-31 21:42:40 +0530

    Add files via upload

---
+++ advitya.cpp
@@ -0,0 +1,16 @@
+//PROBLEM LINK:https://www.codechef.com/START119D/problems/ADVITIYA1
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    if(n==16 or n==17 or n==18){
+        cout<<"ADVITIYA"<<endl;
+    }
+    else{
+        cout<<"WAITING FOR ADVITIYA"<<endl;
+    }
+	// your code goes here
+
+}
