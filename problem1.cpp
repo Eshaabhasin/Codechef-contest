77585718992f1cc56357c9e51659d8c03a6219c8
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2024-02-01 21:37:02 +0530

    Add files via upload

---
+++ problem1.cpp
@@ -0,0 +1,20 @@
+//PROBLEM LINK:https://www.codechef.com/problems/ADVITIYA2
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	// your code goes here
+ int t;
+ cin>>t;
+ while(t--){
+     int r1,r2,r3,r4,r5;
+     cin>>r1>>r2>>r3>>r4>>r5;
+     if(r1+r2+r3+r4+r5>=4){
+         cout<<"YES"<<endl;
+     }
+     else{
+         cout<<"NO"<<endl;
+     }
+    
+ }
+
+}
