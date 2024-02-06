111adfeac764febc8f31df8b4d596ced028203cd
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2024-02-06 21:42:48 +0530

    Add files via upload

---
+++ distribution.cpp
@@ -0,0 +1,14 @@
+//PROBLEM LINK:https://www.codechef.com/problems/CHOCDISTRIB?tab=statement
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<(n+1)/2<<" "<<n<<endl;
+    }
+	// your code goes here
+}
