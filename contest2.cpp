34075cc1ff3a51e226a6dd0d8c8525be38107de8
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2024-02-07 21:49:34 +0530

    Add files via upload

---
+++ contest2.cpp
@@ -0,0 +1,17 @@
+//PROBLEM LINK:https://www.codechef.com/START120D/problems/VAL114
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    if(n==121){
+        cout<<"Likely"<<endl;
+    }
+    else{
+        cout<<"Unlikely"<<endl;
+    }
+    
+	// your code goes here
+
+}
