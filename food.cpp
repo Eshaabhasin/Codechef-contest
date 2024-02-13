f538e193f817c502f08c3ab665fef726c3ca4aa9
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2024-02-13 21:34:15 +0530

    Add files via upload

---
+++ food.cpp
@@ -0,0 +1,11 @@
+//PROBLEM LINK:https://www.codechef.com/problems/FOODCOST
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin>>x>>y;
+    cout<<(x*6)+y;
+	// your code goes here
+
+}
