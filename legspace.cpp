ed98a653d6cee04b7131ce13f6e1d9e5b3bbd6c2
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2024-02-14 21:51:28 +0530

    Add files via upload

---
+++ legspace.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+    if(n<m){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+	// your code goes here
+
+}
