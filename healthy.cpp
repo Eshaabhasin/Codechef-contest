7a078ff498c4fd2a85875ed2de86549da84de171
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2024-02-23 21:15:26 +0530

    Add files via upload

---
+++ healthy.cpp
@@ -0,0 +1,19 @@
+//PROBLEM LINK:https://www.codechef.com/problems/HEALSE
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int h;
+    cin>>h;
+    if(h<8){
+        cout<<"LESS"<<endl;
+    }
+    else if(h==8){
+        cout<<"PERFECT"<<endl;
+    }
+    else{
+        cout<<"MORE"<<endl;
+    }
+	// your code goes here
+
+}
