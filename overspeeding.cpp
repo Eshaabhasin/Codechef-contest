f30a11e8f357c06dc7671e81527d5ed9202caac9
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2024-03-14 21:21:18 +0530

    Add files via upload

---
+++ overspeeding.cpp
@@ -0,0 +1,15 @@
+//PROBLEM LINK:https://www.codechef.com/problems/CCOV
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+    if(x>40){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+
+}
