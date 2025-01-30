bbac08ada703b6211fadf8dbcd6cab0ffbd13943
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2025-01-30 20:58:15 +0530

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,12 @@
+//https://www.codechef.com/problems/SQUIDBANK
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int k,n;
+    cin>>k>>n;
+    int z=k-n;
+    cout<<10000*z<<endl;
+	// your code goes here
+
+}
