559347767277ba2d3754f0cbfa9aeba5b6973ac0
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2025-02-02 21:28:18 +0530

    Add files via upload

---
+++ budget.cpp
@@ -0,0 +1,23 @@
+//https://www.codechef.com/problems/BUDGET_
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        int z;
+        z=y*30;
+        if(z<=x){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+        
+    }
+	// your code goes here
+
+}
