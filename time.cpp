cf72ac0a9fa261217a5c67eed387ca38f8725c2a
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2025-02-10 22:55:01 +0530

    Add files via upload

---
+++ time.cpp
@@ -0,0 +1,17 @@
+//https://www.codechef.com/problems/TIMA
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+    int z=x+25;
+    if(z>=2050){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+	// your code goes here
+
+}
