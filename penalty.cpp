0e293de0cb32f3208442192d7541bb3b619aaede
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2025-02-13 20:12:06 +0530

    Add files via upload

---
+++ penalty.cpp
@@ -0,0 +1,11 @@
+//https://www.codechef.com/problems/WAPEN
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin>>x>>y;
+    cout<<x+y*10<<endl;
+	// your code goes here
+
+}
