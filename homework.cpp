3026beb233d29c76a98240624f831c4150f98338
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2025-02-19 20:57:14 +0530

    Add files via upload

---
+++ homework.cpp
@@ -0,0 +1,17 @@
+//https://www.codechef.com/START174D/problems/HWFIN
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin>>x>>y;
+    int z=y*10;
+    if(z+x>=100){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+	// your code goes here
+
+}
