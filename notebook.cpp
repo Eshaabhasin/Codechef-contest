461ef4587f0832ff2d4d69d87602214e5b7ff87b
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2025-03-04 15:58:44 +0530

    Add files via upload

---
+++ notebook.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/problems/NOTEBOOK
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        int z=x*1000;
+        cout<<z/100<<endl;
+    }
+	// your code goes here
+
+}
