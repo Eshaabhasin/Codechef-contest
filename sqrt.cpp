a835af072fc4335bf47d13e751accd0e2b6e7680
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2025-03-05 16:16:47 +0530

    Add files via upload

---
+++ sqrt.cpp
@@ -0,0 +1,17 @@
+//https://www.codechef.com/problems/FSQRT?tab=statement
+#include <bits/stdc++.h>
+#include<math.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        int z=sqrt(x);
+        cout<<z<<endl;
+    }
+	// your code goes here
+
+}
