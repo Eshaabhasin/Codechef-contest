af60f7b63e759d601bd4154b945338ff348b5ee2
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2025-03-10 21:37:23 +0530

    Add files via upload

---
+++ max.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/problems/DISCUS?tab=submissions
+#include <bits/stdc++.h>
+#include<math.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y,z;
+        cin>>x>>y>>z;
+        cout<<max({x,y,z})<<endl;
+    }
+	// your code goes here
+
+}
