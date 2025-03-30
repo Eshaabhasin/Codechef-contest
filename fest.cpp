c5d310d1697f074fb7a1658c5d100a4a07ee6a13
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2025-03-30 21:19:41 +0530

    Add files via upload

---
+++ fest.cpp
@@ -0,0 +1,17 @@
+//https://www.codechef.com/problems/CLSI
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,b;
+    cin>>n>>b;
+    int difficulty=10*b;
+    if(difficulty<=n){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+	// your code goes here
+
+}
