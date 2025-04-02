0f96af9a56ffbe5eeb1ebd64b1c298d4aa63b43b
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2025-04-02 21:18:06 +0530

    Add files via upload

---
+++ codemat.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/START180D/problems/CDMT
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin>>x>>y;
+    if(x<y){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+	// your code goes here
+
+}
