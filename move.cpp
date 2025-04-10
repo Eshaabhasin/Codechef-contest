3d9a8c8ee9faa7eeec9e30b7c3b6889963faf4d1
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2025-04-10 19:54:01 +0530

    Add files via upload

---
+++ move.cpp
@@ -0,0 +1,11 @@
+//https://www.codechef.com/problems/MOVEMENT?tab=statement
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout<<a-c<<" "<<b-d<<endl;
+	// your code goes here
+
+}
