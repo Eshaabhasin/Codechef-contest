832eae9e4a1c47b5d5c598e89bbee91b545d57b4
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2025-04-17 21:20:29 +0530

    Add files via upload

---
+++ ipl.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/problems/INPL
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+    if(x>=7){
+        cout<<"THALA"<<endl;
+    }
+    else{
+        cout<<"BOOM"<<endl;
+    }
+	// your code goes here
+
+}
