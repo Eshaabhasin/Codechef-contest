82a1d7f879154915f7ae38f8f7b96672449203d3
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2025-06-21 20:31:41 +0530

    Add files via upload

---
+++ playlist.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/problems/SONGS?tab=statement
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        cout<<n/(3*x)<<endl;
+        
+    }
+	// your code goes here
+
+}
