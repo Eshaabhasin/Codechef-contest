9ac8f14ef15dfabeb7a22bd265998c751c1fef1b
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2025-06-25 21:10:45 +0530

    Add files via upload

---
+++ shoes.cpp
@@ -0,0 +1,11 @@
+//https://www.codechef.com/START192D/problems/SHOM
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int l,r;
+    cin>>l>>r;
+    cout<<abs(l-r)<<endl;
+	// your code goes here
+
+}
