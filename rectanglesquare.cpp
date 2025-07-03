186717bc23c185f1e0e7b64eb48d396f2c431844
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2025-07-03 21:06:51 +0530

    Add files via upload

---
+++ rectanglesquare.cpp
@@ -0,0 +1,18 @@
+//https://www.codechef.com/START193D/problems/RECSQ
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    int areasquare=c*c;
+    int arearectangle=a*b;
+    if(areasquare==arearectangle){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+	// your code goes here
+
+}
