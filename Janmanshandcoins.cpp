60419d5e2ae01833208e749f87a5a5e68a08096f
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2025-07-04 20:54:10 +0530

    Add files via upload

---
+++ Janmanshandcoins.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/JCOINS
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        cout<<x*10+y*5<<endl;
+    }
+	// your code goes here
+
+}
